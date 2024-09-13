9416e2a208ded381a06b002066a18902b70361a8
나평근 <skvudrms54@gmail.com>
2024-09-13 16:38:23 +0900

    refactor: change package structure and move class

---
+++ src/main/java/org/itbuddy/security/book/application/BookService.java
@@ -0,0 +1,28 @@
+package org.itbuddy.security.book.application;
+
+import lombok.RequiredArgsConstructor;
+import org.itbuddy.security.book.repository.Book;
+import org.itbuddy.security.book.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository repository;
+
+    public void save(BookRequest request) {
+        var book = Book.builder()
+                       .id(request.getId())
+                       .author(request.getAuthor())
+                       .isbn(request.getIsbn())
+                       .build();
+        repository.save(book);
+    }
+
+    public List<Book> findAll() {
+        return repository.findAll();
+    }
+}
---
+++ src/main/java/org/itbuddy/security/book/ui/BookController.java
@@ -0,0 +1,35 @@
+package org.itbuddy.security.book.ui;
+
+import lombok.RequiredArgsConstructor;
+import org.itbuddy.security.book.application.BookRequest;
+import org.itbuddy.security.book.application.BookService;
+import org.itbuddy.security.book.repository.Book;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService service;
+
+    @PostMapping
+    public ResponseEntity<?> save(
+            @RequestBody BookRequest request
+    ) {
+        service.save(request);
+        return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> findAllBooks() {
+        return ResponseEntity.ok(service.findAll());
+    }
+}
